78d6d07bb18f1d535fc0c0188811d12eb0d451e0
Chicodemilo <mileschick@gmail.com>
2020-10-11 12:38:52 -0500

    cam motor 2

--- camera.py
+++ camera.py
@@ -8,16 +8,16 @@ print(sys.argv[1])
 
 GPIO.setmode(GPIO.BOARD)
 hor = [7, 11, 13, 15]
-# vert = [29, 31, 33, 35]
+vert = [12, 16, 18, 22]
 
 for pin in hor:
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, 0)
 
 
-# for pin in pins_two:
-#     GPIO.setup(pin, GPIO.OUT)
-#     GPIO.output(pin, 0)
+for pin in vert:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, 0)
 
 fore = [
     [1, 0, 0, 1],
@@ -50,15 +50,15 @@ def turnOne():
             time.sleep(0.001)
 
 
-# def openTwo():
-#     for i in range(260):
-#         for halfstep in range(8):
-#             for pin in range(4):
-#                 GPIO.output(control_pins[pin], backwards[halfstep][pin])
-#             time.sleep(0.001)
+def turnTwo():
+    for i in range(260):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(vert[pin], aft[halfstep][pin])
+            time.sleep(0.001)
 
 
 turnOne()
-# openTwo()
+turnTwo()
 
 GPIO.cleanup()
