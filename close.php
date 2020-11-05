8b2f8116b4f249a43597af551aa16a2b5d25096a
Chicodemilo <mileschick@gmail.com>
2020-11-05 12:24:41 -0600

    door logging

--- close.php
+++ close.php
@@ -1,6 +1,6 @@
 <?php
-
-echo ("CLOSED");
+$date = date('Y-m-d h:i:s');
+echo (">>> CLOSED $date \n");
 $command = 'python close.py';
 exec($command, $output);
 header("Location: access.php");
--- run.php
+++ run.php
@@ -1,6 +1,6 @@
 <?php
-
-echo ("OPEN");
+$date = date('Y-m-d h:i:s');
+echo (">>> OPEN $date \n");
 $command = 'python motor.py';
 exec($command, $output);
 header("Location: access.php");
